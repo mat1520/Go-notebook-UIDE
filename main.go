e216c6d7d6c6373b65c80efa7087586fb5deec06
Ariel Matias Melo <azaazay09@gmail.com>
2025-09-01 11:19:35 -0500

    Add SQLite database integration and remove unused user management code

--- main.go
+++ main.go
@@ -1,61 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-var usuarios = make(map[string]string)
-
-func registerUser(Nombre string, Contraseña string) string {
-	fmt.Println("Ingrese su nombre:")
-	fmt.Scanln(&Nombre)
-	fmt.Println("Ingrese su contraseña:")
-	fmt.Scanln(&Contraseña)
-	usuarios[Nombre] = Contraseña
-	return "Usuario registrado: " + Nombre
-}
-
-func iniciarSesion(Nombre string, Contraseña string) string {
-	fmt.Println("Ingrese su nombre:")
-	fmt.Scanln(&Nombre)
-	fmt.Println("Ingrese su contraseña:")
-	fmt.Scanln(&Contraseña)
-	if Nombre == usuarios[Nombre] {
-		return "Usuario iniciado sesión: " + Nombre
-	}
-	return "Error: usuario o contraseña incorrecta"
-}
-
-func loginMenu() {
-	fmt.Println("1. Iniciar sesión")
-	fmt.Println("2. Registrarse")
-	fmt.Print("Seleccione una opción: ")
-}
-
 func main() {
-
-	var Nombre string
-	var Contraseña string
-
-	fmt.Println("Bienvenido, Por favor registrate o inicia sesión.")
-	for {
-		// Código que se ejecutará para siempre hasta que se use 'break'
-		loginMenu()
-		var opcion int
-		fmt.Scanln(&opcion)
-		switch opcion {
-		case 1:
-			result := iniciarSesion(Nombre, Contraseña)
-			fmt.Println(result)
-			if result[:24] == "Usuario iniciado sesión:" {
-				fmt.Printf("Listo para transferir tu dinero, %s\n", Nombre)
-				break
-			}
-		case 2:
-			fmt.Println(registerUser(Nombre, Contraseña))
-		default:
-			fmt.Println("Opción no válida")
-		}
+	db, err := sql.Open("sqlite3", "./users.db")
+	if err != nil {
+		fmt.Println("Error al abrir la base de datos:", err)
+		return
 	}
+	defer db.Close()
 
+	fmt.Println("Base de datos y tabla listas.")
 }
