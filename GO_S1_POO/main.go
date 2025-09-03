223f93cbfff5b2b8c7f1a4700e5315ba52e87872
Ariel Matias Melo <azaazay09@gmail.com>
2025-09-03 12:00:34 -0500

    Add age input and update greeting message in main function

--- GO_S1_POO/main.go
+++ GO_S1_POO/main.go
@@ -6,7 +6,10 @@ import (
 
 func main() {
 	var nombre string
+	var edad int
 	fmt.Println("Ingrese su nombre:")
 	fmt.Scanln(&nombre)
-	fmt.Printf("Hello, %s!\n", nombre)
+	fmt.Println("Ingrese su edad:")
+	fmt.Scanln(&edad)
+	fmt.Printf("Hello, %s! You are %d years old.\n", nombre, edad)
 }
